da87b9df1abbdee3a3efba43a4e0bc82ea8f6e6b
vhthien611 <thien.vh611@gmail.com>
2024-10-18 17:26:47 +0700

    Update CSS

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,29 +1,7 @@
-import { Button } from './components';
-
 function App() {
 	return (
 		<div className='text-base w-full h-screen flex-center flex-col gap-6'>
 			<div className='font-bold'>@starter-ui/core</div>
-			<div className='flex-center w-80 flex-wrap gap-3 overflow-auto rounded-md border p-3'>
-				<Button isLoading outline>
-					Default
-				</Button>
-				<Button isLoading outline color='primary'>
-					Primary
-				</Button>
-				<Button isLoading outline color='secondary'>
-					Secondary
-				</Button>
-				<Button isLoading outline color='success'>
-					Success
-				</Button>
-				<Button isLoading outline color='warning'>
-					Warning
-				</Button>
-				<Button isLoading outline color='error'>
-					Error
-				</Button>
-			</div>
 		</div>
 	);
 }
